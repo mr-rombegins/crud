4a9ca2c6d2c2547d4c0edcd3b72bc27824604dfe
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-04-19 14:10:38 +0000

    Backend CRUD

---
+++ config/dbConnect.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/blog");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/blog", {
+    //   dbName: "blog",
+    // });
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  dbConnect,
+};
